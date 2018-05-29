d05c842d5ba287a90721713c0505a75cc907149a
leonardo.mlmendes <Leonardo.mlmendes@gmail.com>
2018-05-29 17:09:59 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/orange/SignActivity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.orange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_layout);
+    }
+}
